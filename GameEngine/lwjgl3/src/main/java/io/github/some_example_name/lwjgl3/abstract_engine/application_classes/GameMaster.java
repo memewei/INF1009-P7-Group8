b96a516816c6ea96e87b9ac77941994aaa5d8f22
limgreg <2401480@sit.singaporetech.edu.sg>
2025-02-14 01:56:07 +0800

    Task completed 1. Move Game master into applciation classes

--- GameEngine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_engine/application_classes/GameMaster.java
+++ GameEngine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_engine/application_classes/GameMaster.java
@@ -1,4 +1,4 @@
-package io.github.some_example_name.lwjgl3.abstract_engine;
+package io.github.some_example_name.lwjgl3.abstract_engine.application_classes;
 
 import com.badlogic.gdx.ApplicationAdapter;
 
