b44baf8b9339237dc0afe681b0d9253186db92a2
Si Pei <tohsipei@yahoo.com.sg>
2025-02-18 13:19:16 +0800

    Imported IOManager into CollisionListener and implemented hit_sound effects whenever stuff collides

--- GameEngine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_engine/collision/Box2DCollisionListener.java
+++ GameEngine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_engine/collision/Box2DCollisionListener.java
@@ -2,6 +2,7 @@ package io.github.some_example_name.lwjgl3.abstract_engine.collision;
 
 import com.badlogic.gdx.physics.box2d.*;
 import io.github.some_example_name.lwjgl3.abstract_engine.entity.Entity;
+import io.github.some_example_name.lwjgl3.abstract_engine.io.IOManager;
 
 public class Box2DCollisionListener implements ContactListener {
 
@@ -17,10 +18,12 @@ public class Box2DCollisionListener implements ContactListener {
             if (entityA instanceof Collidable) {
                 ((Collidable) entityA).onCollision(entityB);
                 System.out.println(entityA.getEntityName() + " collided with " + entityB.getEntityName());
+                IOManager.getInstance().getAudio().playSound("hit_sound.mp3");
             }
             if (entityB instanceof Collidable) {
                 ((Collidable) entityB).onCollision(entityA);
                 System.out.println(entityB.getEntityName() + " collided with " + entityA.getEntityName());
+                IOManager.getInstance().getAudio().playSound("hit_sound.mp3");
             }
         }
     }
