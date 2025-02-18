fd9d386b188887d6a4e6a5e27d1e5bcbab646380
Si Pei <tohsipei@yahoo.com.sg>
2025-02-18 23:28:43 +0800

    Revert "Added interface and new class to handle gdx inputs to adhere to DIP."
    This reverts commit fcc4eebad2d36baaad1fadbf9fe7ccf854d7a4e0.

--- GameEngine/lwjgl3/src/main/java/io/github/some_example_name/lwjgl3/abstract_engine/io/InputHandler.java
+++
@@ -1,6 +0,0 @@
-package io.github.some_example_name.lwjgl3.abstract_engine.io;
-
-public interface InputHandler {
-    boolean isKeyPressed(int keycode);
-    boolean isKeyJustPressed(int keycode);
-}
